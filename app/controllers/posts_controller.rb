4331d6b82447785a2635c215799474d73ab98871
Rafael Pinto <pintorafael1999@gmail.com>
2025-02-17 15:30:19 +0000

    Routes, posts controller index

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,5 @@
 class PostsController < ApplicationController
+  def index
+    @posts = Post.includes(:user, :comments, :likes).order(created_at: :desc)
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,15 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  # root "posts#index"
+  resources :posts do
+    resources :comments, only: [:create, :destroy] # Nested for post comments
+    resources :likes, only: [:create, :destroy]    # Nested for post likes
+    resources :bookmarks, only: [:create, :destroy]
+  end
+
+  resources :albums, only: [:index, :show, :create, :destroy]
+  resources :messages, only: [:index, :create, :destroy]
+  resources :relationships, only: [:create, :destroy]
+
+  root "posts#index"  # Set homepage to show posts
 end
